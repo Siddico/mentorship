98d30073b610479fd47a2d3b7d4883128a7c94fc
Siddico <www.mohammedahmedmohammedsiddiq@gmail.com>
2025-09-09 21:50:39 +0200

    finish ui 95% after edit images

--- lib/utils/constants/app_assets.dart
+++ lib/utils/constants/app_assets.dart
@@ -1,26 +1,26 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
+
   /// Assets for assetsImagesApple
   /// assets/images/Apple.png
   static const String assetsImagesApple = "assets/images/Apple.png";
 
   /// Assets for assetsImagesBannerFourthImage
   /// assets/images/bannerFourthImage.png
-  static const String assetsImagesBannerFourthImage = "assets/images/bannerFourthImage.png";
+  static const String assetsImagesBannerFourthImage = "assets/images/man1.png";
 
   /// Assets for assetsImagesBannerImage
   /// assets/images/bannerImage.png
-  static const String assetsImagesBannerImage = "assets/images/bannerImage.png";
+  static const String assetsImagesBannerImage = "assets/images/man22.png";
 
   /// Assets for assetsImagesBannerSecondImage
   /// assets/images/bannerSecondImage.png
-  static const String assetsImagesBannerSecondImage = "assets/images/bannerSecondImage.png";
+  static const String assetsImagesBannerSecondImage = "assets/images/man3.png";
 
   /// Assets for assetsImagesBannerThirdImage
   /// assets/images/bannerThirdImage.png
-  static const String assetsImagesBannerThirdImage = "assets/images/bannerThirdImage.png";
+  static const String assetsImagesBannerThirdImage = "assets/images/man4.png";
 
   /// Assets for assetsImagesFacebook
   /// assets/images/Facebook.png
@@ -28,7 +28,7 @@ class Assets {
 
   /// Assets for assetsImagesFirstonboarding
   /// assets/images/firstonboarding.png
-  static const String assetsImagesFirstonboarding = "assets/images/firstonboarding.png";
+  static const String assetsImagesFirstonboarding = "assets/images/man1.png";
 
   /// Assets for assetsImagesGoogle
   /// assets/images/Google.png
@@ -36,54 +36,55 @@ class Assets {
 
   /// Assets for assetsImagesOnboarding1
   /// assets/images/onboarding1.svg
-  static const String assetsImagesOnboarding1 = "assets/images/onboarding1.svg";
+  static const String assetsImagesOnboarding1 = "assets/images/man22.svg";
 
   /// Assets for assetsImagesOnboarding2
   /// assets/images/onboarding2.svg
-  static const String assetsImagesOnboarding2 = "assets/images/onboarding2.svg";
+  static const String assetsImagesOnboarding2 = "assets/images/man3.svg";
 
   /// Assets for assetsImagesOnboarding3
   /// assets/images/onboarding3.svg
-  static const String assetsImagesOnboarding3 = "assets/images/onboarding3.svg";
+  static const String assetsImagesOnboarding3 = "assets/images/man4.svg";
 
   /// Assets for assetsImagesPerson1
   /// assets/images/person1.png
-  static const String assetsImagesPerson1 = "assets/images/person1.png";
+  static const String assetsImagesPerson1 = "assets/images/man1.png";
 
   /// Assets for assetsImagesPerson2
   /// assets/images/person2.png
-  static const String assetsImagesPerson2 = "assets/images/person2.png";
+  static const String assetsImagesPerson2 = "assets/images/man22.png";
 
   /// Assets for assetsImagesPerson3
   /// assets/images/person3.png
-  static const String assetsImagesPerson3 = "assets/images/person3.png";
+  static const String assetsImagesPerson3 = "assets/images/man3.png";
 
   /// Assets for assetsImagesSecondonboarding
   /// assets/images/secondonboarding.png
-  static const String assetsImagesSecondonboarding = "assets/images/secondonboarding.png";
+  static const String assetsImagesSecondonboarding = "assets/images/man4.png";
 
   /// Assets for assetsImagesSplashviewbackground
   /// assets/images/splashviewbackground.svg
-  static const String assetsImagesSplashviewbackground = "assets/images/splashviewbackground.svg";
+  static const String assetsImagesSplashviewbackground =
+      "assets/images/man1.svg";
 
   /// Assets for assetsImagesSplashviewbackgroundd
   /// assets/images/splashviewbackgroundd.png
-  static const String assetsImagesSplashviewbackgroundd = "assets/images/splashviewbackgroundd.png";
+  static const String assetsImagesSplashviewbackgroundd =
+      "assets/images/man4.png";
 
   /// Assets for assetsImagesThirdonboarding
   /// assets/images/thirdonboarding.png
-  static const String assetsImagesThirdonboarding = "assets/images/thirdonboarding.png";
+  static const String assetsImagesThirdonboarding = "assets/images/man22.png";
 
   /// Assets for assetsImagesWahdaneswan1
   /// assets/images/wahdaneswan1.png
-  static const String assetsImagesWahdaneswan1 = "assets/images/wahdaneswan1.png";
+  static const String assetsImagesWahdaneswan1 = "assets/images/man3.png";
 
   /// Assets for assetsImagesWahdaneswan2
   /// assets/images/wahdaneswan2.png
-  static const String assetsImagesWahdaneswan2 = "assets/images/wahdaneswan2.png";
+  static const String assetsImagesWahdaneswan2 = "assets/images/man1.png";
 
   /// Assets for assetsImagesWahdaneswan3
   /// assets/images/wahdaneswan3.png
-  static const String assetsImagesWahdaneswan3 = "assets/images/wahdaneswan3.png";
+  static const String assetsImagesWahdaneswan3 = "assets/images/man4.png";
 }
-
